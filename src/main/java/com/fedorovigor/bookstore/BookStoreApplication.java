f8c55744c38ccc250674931bfe18d9fbcdc2eb26
igor fedorov <fedorovigord@yandex.ru>
2022-04-17 20:30:22 +0300

    session repo was changed - all related data stored in Redis.

--- src/main/java/com/fedorovigor/bookstore/BookStoreApplication.java
+++ src/main/java/com/fedorovigor/bookstore/BookStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizeApplication {
+public class BookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthorizeApplication.class, args);
+		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
 }
